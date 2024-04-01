92457b50c4ff560307fb5a1128168d01dd8cad78
Saarthak Parulekar <119943304+saarthakparulekar@users.noreply.github.com>
2024-04-01 23:32:55 +0530

    Create lenLastWord.js

---
+++ lenLastWord.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let count = 0;
+    for(let i = s.length-1; i >= 0; i--){
+        if(s[i] ==" "){
+            if(count > 0){
+            return count;
+            }
+        }else{
+            count++;
+        }
+    };
+    return count;
+};
