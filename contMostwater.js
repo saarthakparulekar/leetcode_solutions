ecafc44e4077c44364c175e38d00f5c4b658ea56
Saarthak Parulekar <119943304+saarthakparulekar@users.noreply.github.com>
2024-08-05 00:33:07 +0530

    Create contMostwater.js

---
+++ contMostwater.js
@@ -0,0 +1,18 @@
+//Revise in a week
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let i = 0;
+    let j = height.length-1;
+    let maxArea = 0;
+    while(i < j){
+        let area = Math.min(height[i],height[j]) * (j - i);
+        maxArea = Math.max(area,maxArea);
+        if(height[i] < height[j]){
+            i++;
+        }else j--;
+    }
+    return maxArea;
+};
