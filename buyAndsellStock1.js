1566970ffba1d6bd1809174a0b26c72001eb5742
Saarthak Parulekar <119943304+saarthakparulekar@users.noreply.github.com>
2024-08-05 19:30:13 +0530

    Update buyAndsellStock1.js

--- buyAndsellStock1.js
+++ buyAndsellStock1.js
@@ -1,3 +1,24 @@
+//Solved on own
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let max = 0;
+    let low = prices[0];
+    let j = 1;
+    while(j < prices.length){
+        if(prices[j] < low){
+            low = prices[j];
+        }else{
+            let profit = prices[j] - low;
+            max = Math.max(max,profit);
+        }
+        j++;
+    }
+    return max;
+};
+//solved with help
 /**
  * @param {number[]} prices
  * @return {number}
