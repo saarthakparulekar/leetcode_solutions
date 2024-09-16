322c95531a2237c25ea4d2214269935088bd429b
Saarthak Parulekar <119943304+saarthakparulekar@users.noreply.github.com>
2024-09-16 16:23:18 +0530

    Create binarySearch.js

---
+++ binarySearch.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let l = 0;
+    let r = nums.length - 1;
+    while(l <= r){
+        let mid = l + Math.floor((r-l)/2);
+        if(nums[mid] > target){
+            r = mid-1;
+        }else if(nums[mid] < target){
+            l = mid + 1;
+        }else{
+            return mid;
+        }
+    }
+    return -1;
+};
