9ad5eac335e3af0ee74fef548fb90b0092fd4f64
Saarthak Parulekar <119943304+saarthakparulekar@users.noreply.github.com>
2024-12-30 14:55:40 +0530

    Update findDuplicate.js

--- findDuplicate.js
+++ findDuplicate.js
@@ -1,3 +1,25 @@
+//With slow and fast pointer
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    let slow = nums[0];
+    let fast = nums[nums[0]];
+    while(slow != fast){
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    }
+    fast = 0;
+    while(slow != fast){
+        slow = nums[slow];
+        fast = nums[fast];
+    };
+    return slow;
+};
+
+//the solution below is stupid
+
 var findDuplicate = function(nums) {
  let duplicate = new Set()
     for(let num  of nums){
