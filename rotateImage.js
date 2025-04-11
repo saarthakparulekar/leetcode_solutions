5c61b0c2304b1be42e1b06279f5799c18429d410
Saarthak Parulekar <119943304+saarthakparulekar@users.noreply.github.com>
2025-04-11 12:47:18 +0530

    Create rotateImage.js

---
+++ rotateImage.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    for(let r = 0; r < matrix.length; r++){
+        for(let c = r; c < matrix[0].length; c++){
+            let temp = matrix[r][c];
+            matrix[r][c] = matrix[c][r];
+            matrix[c][r] = temp;
+        }
+    }
+    for(let r = 0; r < matrix.length; r++){
+        matrix[r].reverse()
+    }
+};
